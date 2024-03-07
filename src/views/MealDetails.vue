81049f984e9cc3e55d7aaaa8b46350544603cf6f
Pra7iksinh <pra7iksinh@gmail.com>
2024-03-08 00:56:39 +0530

    Meal by id functionality and page

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed, onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router';
+import { useRoute, RouterLink } from 'vue-router';
 import store from '../store';
 
 
@@ -29,7 +29,7 @@ function getMealByID() {
 
 <template>
     <div class="p-10">
-        <div class="rounded-2xl bg-white p-10">
+        <div class="rounded-2xl bg-white p-5 pt-10">
             <div class="flex justify-center">
                 <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-xl">
             </div>
@@ -37,25 +37,58 @@ function getMealByID() {
                 <h1 class="items-center  text-orange-500 rounded-3xl pt-8 text-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:text-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 dark:focus:ring-orange-800 font-bold text-3xl">{{ meal.strMeal }}</h1>
             </div>
             <hr class="m-5">
-            <div class="flex justify-evenly text-2xl pr-20">
-                <div><span class="text-cyan-500 font-semibold"> Category: </span>{{ meal.strCategory }}</div>
-                <div><span class="text-cyan-500 font-semibold"> Area: </span>{{ meal.strArea }}</div>
-                <div><span class="text-cyan-500 font-semibold"> Tags: </span>{{ meal.strTags }}</div>
+            <div class="grid sm:grid-cols-1 md:sm:grid-cols-3 text-center text-2xl">
+                <div>
+                    <span class="text-cyan-500 font-semibold"> Category: </span>
+                    {{ meal.strCategory }}
+                </div>
+                <div>
+                    <span class="text-cyan-500 font-semibold"> Area: </span>
+                    {{ meal.strArea }}
+                </div>
+                <div>
+                    <span class="text-cyan-500 font-semibold"> Tags: </span>
+                    {{ meal.strTags }}
+                </div>
             </div>
             <hr class="m-5"/>
             <div class="flex justify-center">
                 <h1 class="font-bold text-2xl text-cyan-500">Instruction</h1>
             </div>
-            <div class="text-xl pr-20 indent-20 w-full pl-20 pt-2 text-pretty">
+            <div class="text-xl pr-20 md:indent-10 sm:indent-0 w-full pl-20 pt-2 text-pretty">
                 {{ meal.strInstructions }}
             </div>
             <hr class="m-5">
+            <div class="grid grid-cols-1 sm:grid-cols-1 md:sm:grid-cols-2">
+                <div class="text-center border-none sm:border-none md:border-solid border-r mb-2">
+                    <h1 class="font-bold text-2xl text-cyan-500">Ingredients</h1>
+                    <ul>
+                        <template v-for="(element, index) of new Array(20)" >
+                            <li v-if="meal[`strIngredient${index+1}`] !== ''" class="text-lg">
+                                {{ index+1}}: {{ meal[`strIngredient${index+1}`] }}
+                            </li>
+                        </template>
+                    </ul>
+                </div>
+                <hr class="visible md:hidden mt-2 mb-2">
+                <div class="text-center border-none sm:border-none md:border-solid border-l mt-2">
+                    <h1 class="font-bold text-2xl text-cyan-500">Measures</h1>
+                    <ul>
+                        <template v-for="(element, index) of new Array(20)" >
+                            <li v-if="meal[`strMeasure${index+1}`] !== ''" class="text-lg">
+                                {{ index+1}}: {{ meal[`strMeasure${index+1}`] }}
+                            </li>
+                        </template>
+                    </ul>
+                </div>
+            </div>
+            <hr class="m-5">
+            <div class="pb-2 flex gap-5 sm:flex-row md:flex-row flex-col">
+                <a :href="meal.strYoutube" target="_blank" class=" text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-2xl px-5 py-2.5 text-center me-2 mb-2">YouTube</a>
+                <a :href="meal.strSource" target="_blank" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-2xl px-5 py-2.5 text-center me-2 mb-2">View Original Source</a>
+            </div>
         </div>
-
     </div>
-    <pre>
-        {{ meal }}
-    </pre>
 </template>
 
 <style scoped></style>
--- src/views/SearchByName.vue
+++ src/views/SearchByName.vue
@@ -37,7 +37,7 @@ onMounted(() => {
             </RouterLink>
             <div class="px-2 pt-2">
                 <h3 class="p-2 font-semibold">{{ meal.strMeal }}</h3>
-                <p class="mb-5"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi adipisci illum molestiae, ipsa saepe, perferendis vitae at eum voluptates minus veniam dicta unde, omnis cupiditate velit neque officia aperiam. Ad.</p>
+                <!-- <p class="mb-5">{{ meal.strInstructions }}</p> -->
                 <div class="pb-2 m-2">
                     <a :href="meal.strYoutube" target="_blank" class=" text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">YouTube</a>
                     <RouterLink to="/" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
